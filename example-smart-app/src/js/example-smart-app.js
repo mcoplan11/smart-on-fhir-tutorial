7465714f023802ac65334e01431c9a04f3d55895
sarmadtanvir <50720999+sarmadtanvir@users.noreply.github.com>
2019-05-17 08:36:14 +0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -60,7 +60,7 @@
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
-          p.trig = getQuantityValueAndUnit(trig[0]);
+          p.trig = 'av';
 
           ret.resolve(p);
         });
