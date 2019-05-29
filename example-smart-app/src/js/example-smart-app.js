6be31da6a7c3e72c5531ed7b578f912064694f08
johnalcantaracom <46574331+johnalcantaracom@users.noreply.github.com>
2019-05-29 11:20:36 +0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -39,6 +39,7 @@
           }
 
           var height = byCodes('8302-2');
+          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
@@ -58,7 +59,10 @@
           p.chol = getQuantityValueAndUnit(chol[0]);
           p.hemog = getQuantityValueAndUnit(hemog[0]);
 
-
+          if (typeof systolicbp != 'undefined')  {
+            p.systolicbp = systolicbp;
+          }
+          
           if (typeof diastolicbp != 'undefined') {
             p.diastolicbp = diastolicbp;
           }
