12e7f1d14c84b5ec9d92e3f1c1df7ed58e25ae5d
johnalcantaracom <46574331+johnalcantaracom@users.noreply.github.com>
2019-05-29 11:15:56 +0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -53,7 +53,7 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
-          p.trig = getQuantityValueAndUnit(trig[0]);
+          p.trig = getQuantityValueAndUnit(trig);
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
           p.chol = getQuantityValueAndUnit(chol[0]);
@@ -62,7 +62,7 @@
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
           }
-          
+
           if (typeof diastolicbp != 'undefined') {
             p.diastolicbp = diastolicbp;
           }
